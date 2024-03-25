a6ca0960788d5d126e2c16c9cf325402d2fddd12
IvoGarraza <ivogarrachip@gmail.com>
2024-03-25 17:20:56 -0300

    links en footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -19,14 +19,14 @@ const Footer = () => {
             </h5> */}
             <div class="mt-6 lg:mb-0 mb-6">
               <a
-                href=""
+                href="https://www.facebook.com/profile.php?id=100084719874739"
                 class="bg-white text-lightBlue-400 shadow-lg font-normal h-10 w-10 items-center justify-center align-center rounded-full outline-none focus:outline-none mr-2"
                 type="button"
               >
                 <img src={facebook}></img>
               </a>
               <a
-              href=""
+              href="https://www.instagram.com/webdev_arg/"
                 class="bg-white text-lightBlue-600 shadow-lg font-normal h-10 w-10 items-center justify-center align-center rounded-full outline-none focus:outline-none mr-2"
                 type="button"
               >
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -62,10 +62,10 @@ const Home = () => {
         <span>
           {t("home.subtittle")}
         </span>
-        <div className="mt-4 ">
+        <div className="mt-4">
           <a
             href="#Form"
-            class="relative inline-flex items-center px-10 py-3 overflow-hidden text-lg font-medium text-yellow-400 border-2 border-yellow-400 rounded-full hover:text-white group hover:bg-gray-50"
+            class="relative inline-flex items-center sm:px-10 px-8 py-3 overflow-hidden text-lg font-medium text-yellow-400 border-2 border-yellow-400 rounded-full hover:text-white group hover:bg-gray-50"
           >
             <span class="absolute left-0 block w-full h-0 transition-all bg-yellow-400 opacity-100 group-hover:h-full top-1/2 group-hover:top-0 duration-400 ease"></span>
             <span class="absolute right-0 flex items-center justify-start w-10 h-10 duration-300 transform translate-x-full group-hover:translate-x-0 ease"></span>
